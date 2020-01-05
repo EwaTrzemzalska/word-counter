4503fea8d8a18f2829398c0c52084421b94e684b
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-05 18:23:28 +0100

    Adding first tests

--- test/word_counter/core_test.clj
+++ test/word_counter/core_test.clj
@@ -1,7 +1,20 @@
 (ns word-counter.core-test
   (:require [clojure.test :refer :all]
-            [word-counter.core :refer :all]))
+            [word-counter.core :as core]))
+
+
+(deftest counting-words
+  (is (= 1 (core/count-words "Hello")))
+  (is (= 2 (core/count-words "Hello, world!")))
+  (is (= 0 (core/count-words "")))
+  ;; TODO throw Error
+  (is (= 0 (core/count-words 1)))
+  (is (= 0 (core/count-words nil)))
+  )
+
+
+
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
